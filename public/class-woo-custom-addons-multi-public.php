b18a868f0486754649359b7761938b5136bab628
John Sanders <john.sanders@warnermedia.com>
2021-07-15 15:06:38 +0100

    Mostly working

--- public/class-woo-custom-addons-multi-public.php
+++ public/class-woo-custom-addons-multi-public.php
@@ -50,46 +50,32 @@ class Woo_Custom_Addons_Multi_Public
 	 */
 	public function __construct($plugin_name, $version)
 	{
-
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 	}
-
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_styles()
 	{
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Woo_Custom_Addons_Multi_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Woo_Custom_Addons_Multi_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/woo-custom-addons-multi-public.css', array(), $this->version, 'all');
+		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'js/addons-form/dist/main.css', array(), $this->version, 'all');
+		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/style.css', array(), $this->version, 'all');
 	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_scripts()
 	{
 		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/addons-form/dist/index.js', [], $this->version, false);
 	}
-
 	public function inject_seats_form()
 	{
-		echo '<div id="addonsForm">123123aaa</div>';
+		echo '<div id="addonsForm"></div>';
+	}
+	public function add_to_cart()
+	{
+		$cart = WC()->cart->get_cart();
+		echo json_encode($cart);
+	}
+	public function cart_item_quantity($product_quantity)
+	{
+		$pattern = '/value="(.+?)"/';
+		preg_match($pattern, $product_quantity, $matches);
+		if ($matches[1]) return $matches[1];
+		return $product_quantity;
 	}
 }
